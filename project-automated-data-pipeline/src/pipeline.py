ba9cb7e58dec31810db87e744751ad7560ca2784
danielsj2022 <Jeremiahdaniels2004@gmail.com>
2026-04-08 20:54:04 -0400

    solve merge conflict allowing file to run

--- project-automated-data-pipeline/src/pipeline.py
+++ project-automated-data-pipeline/src/pipeline.py
@@ -1,3 +1,7 @@
+import json
+import os
+from datetime import datetime
+import sqlite3
 import requests
 import openmeteo_requests
 import requests_cache
@@ -8,9 +12,8 @@ from retry_requests import retry
 def main():
     multiple_city_weather_report = api_extract()
 
-    save_raw_json(data)
-
-    df = transform_to_dataframe(data)
+    df = transform_to_dataframe(multiple_city_weather_report)  
+    
     save_to_csv(df)
     save_to_sqlite(df)
     
@@ -27,7 +30,6 @@ def api_extract() -> dict[str, dict[str, any]]:
         (40.7143, -74.006, "New York")      
     ]
     url = "https://api.open-meteo.com/v1/forecast"
-    #all_items = []
     cities_forecast = {}
 
     for lat, lon, city in locations:
@@ -52,6 +54,7 @@ def api_extract() -> dict[str, dict[str, any]]:
             data = response.json()
 
             record = {
+                "city": city,
                 "current_temp": data.get("current", {}).get("temperature_2m", -999),
                 "seven_days": data.get("daily", {}).get("time", []),
                 "seven_day_hourly_temp": data.get("hourly", {}).get("temperature_2m", []),
@@ -74,52 +77,10 @@ def api_extract() -> dict[str, dict[str, any]]:
     return cities_forecast
     
 
-if __name__ == "__main__":
-    main()
+def transform_to_dataframe(data): 
+        return pd.DataFrame.from_dict(data, orient='index')
 
-        response = requests.get(url, params = params, timeout = 10)
-        response.raise_for_status()
-        data = response.json()
-        #all_items.append(data)
-
-        record = {
-            "current_temp": data.get("current", {}).get("temperature_2m", -999),
-            "seven_days": data.get("daily", {}).get("time", []),
-            "seven_day_hourly_temp": data.get("hourly", {}).get("temperature_2m", []),
-            "seven_day_daily_temp_max": data.get("daily", {}).get("temperature_2m_max", []),
-            "seven_day_daily_temp_min": data.get("daily", {}).get("temperature_2m_min", []),
-            "seven_day_precipitation_probability": data.get("daily", {}).get("precipitation_probability_max", [])
-        }
-        cities_forecast[city] = record
-
-    # for d in cities_forecast.values():
-    #     print(d)
-    #     print("----------------------------------------------")
-    return cities_forecast
-
-def save_raw_json(data):
-    os.makedirs("data/raw", exist_ok=True)
-    filename = f"data/raw/weather_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
-
-    with open(filename, "w") as f:
-        json.dump(data, f, indent=4)
-
-    print("Raw JSON saved.")
-
-def transform_to_dataframe(data):
-    rows = []
-
-    for city, info in data.items():
-        for i in range(len(info["dates"])):
-            rows.append({
-                "city": city,
-                "date": info["dates"][i],
-                "temp_max": info["temp_max"][i],
-                "temp_min": info["temp_min"][i],
-                "precip": info["precip"][i]
-            })    
-
-def save_to_csv(df):
+def save_to_csv(df):    
     os.makedirs("data/processed", exist_ok=True)
     file_path = "data/processed/7day_weather.csv"
 
@@ -133,8 +94,13 @@ def save_to_csv(df):
 def save_to_sqlite(df):
     os.makedirs("data/processed", exist_ok=True)
     db_path = "data/processed/weather_data.db"
-
     conn = sqlite3.connect(db_path)
+
+    df["seven_days"] = df["seven_days"].apply(lambda x: str(x)) # Convert list to string for SQLite storage
+    df["seven_day_hourly_temp"] = df["seven_day_hourly_temp"].apply(lambda x: str(x))
+    df["seven_day_daily_temp_max"] = df["seven_day_daily_temp_max"].apply(lambda x: str(x))
+    df["seven_day_daily_temp_min"] = df["seven_day_daily_temp_min"].apply(lambda x: str(x))
+    df["seven_day_precipitation_probability"] = df["seven_day_precipitation_probability"].apply(lambda x: str(x))
 
     df.to_sql(
         "weather",
@@ -143,61 +109,17 @@ def save_to_sqlite(df):
         index=False
     )
 
-    conn.close()
+    # cursor = conn.cursor()
+    # cursor.execute("SELECT * FROM weather")
+    # rows = cursor.fetchall()
+    # print("from db")
+    # for r in rows:
+    #     print(r)
 
+    conn.close()
     print(f"Saved {len(df)} rows to SQLite.")
 
 if __name__ == "__main__":
     main()
 
-    """
-    # Process 3 locations
-for response in responses:
-	print(f"\nCoordinates: {response.Latitude()}°N {response.Longitude()}°E")
-	print(f"Elevation: {response.Elevation()} m asl")
-	print(f"Timezone: {response.Timezone()}{response.TimezoneAbbreviation()}")
-	print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
-	
-	# Process current data. The order of variables needs to be the same as requested.
-	current = response.Current()
-	current_temperature_2m = current.Variables(0).Value()
-	
-	print(f"\nCurrent time: {current.Time()}")
-	print(f"Current temperature_2m: {current_temperature_2m}")
-	
-	# Process hourly data. The order of variables needs to be the same as requested.
-	hourly = response.Hourly()
-	hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-	
-	hourly_data = {"date": pd.date_range(
-		start = pd.to_datetime(hourly.Time() + response.UtcOffsetSeconds(), unit = "s", utc = True),
-		end =  pd.to_datetime(hourly.TimeEnd() + response.UtcOffsetSeconds(), unit = "s", utc = True),
-		freq = pd.Timedelta(seconds = hourly.Interval()),
-		inclusive = "left"
-	)}
-	
-	hourly_data["temperature_2m"] = hourly_temperature_2m
-	
-	hourly_dataframe = pd.DataFrame(data = hourly_data)
-	print("\nHourly data\n", hourly_dataframe)
-	
-	# Process daily data. The order of variables needs to be the same as requested.
-	daily = response.Daily()
-	daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
-	daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
-	daily_precipitation_probability_max = daily.Variables(2).ValuesAsNumpy()
-	
-	daily_data = {"date": pd.date_range(
-		start = pd.to_datetime(daily.Time() + response.UtcOffsetSeconds(), unit = "s", utc = True),
-		end =  pd.to_datetime(daily.TimeEnd() + response.UtcOffsetSeconds(), unit = "s", utc = True),
-		freq = pd.Timedelta(seconds = daily.Interval()),
-		inclusive = "left"
-	)}
-	
-	daily_data["temperature_2m_max"] = daily_temperature_2m_max
-	daily_data["temperature_2m_min"] = daily_temperature_2m_min
-	daily_data["precipitation_probability_max"] = daily_precipitation_probability_max
-	
-	daily_dataframe = pd.DataFrame(data = daily_data)
-	print("\nDaily data\n", daily_dataframe)
-    """
+    
