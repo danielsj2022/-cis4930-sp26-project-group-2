9dbc9dda15e3a66de7a1995221173d521f104568
danielsj2022 <Jeremiahdaniels2004@gmail.com>
2026-04-19 18:35:27 -0400

    create home view and template

--- project-full-stack-web-api/mysite/core/views.py
+++ project-full-stack-web-api/mysite/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    context = {"message": "The real world context behind the dataset is that it explores the relationship between phone usage, sleep patterns, and stress levels. With there being a drastic increase in use of smartphones and usage of them, it is important to determine the affects they have on health."}
+    return render(request, "core/home.html", context=context)
+
