c1f772c381df50b6a41863879735e157dac79428
danielsj2022 <Jeremiahdaniels2004@gmail.com>
2026-04-23 15:19:56 -0400

    bring urls in scope of app

--- project-full-stack-web-api/mysite/core/urls.py
+++ project-full-stack-web-api/mysite/core/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 from . import views
+from core.views import RecordListView, RecordDetailView
+
 
 urlpatterns = [
+    path("", views.home, name = "home"),
+    path("records/", RecordListView.as_view(), name = "records"),
+    path("records/<int:pk>/", RecordDetailView.as_view(), name = "record_detail"),
+
+    path("records/add/", views.create_record, name = "create_record"),
+    path("records/<int:pk>/edit/", views.update_record, name = "record_edit"),
+    path("records/<int:pk>/delete/", views.delete_record, name = "record_delete"),
+
+    path("analytics/", views.analytics, name="analytics"),
+    path("fetch/", views.fetch_weather, name="fetch_weather"),
     path('analytics/', views.analytics, name='analytics'),
     path('fetch/', views.fetch_weather, name='fetch_weather'),
 ]
